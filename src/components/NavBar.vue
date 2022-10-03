bd80167dbd1e120220d06af7dacdf1b14d885079
LucasCtrl <lucasalt@protonmail.com>
2022-10-03 20:33:17 +0200

    🚧 Add token form

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mb-4 pb-1 border-b border-solid border-gray-300">
-    <router-link to="/" class="px-4 cursor-pointer"> Home </router-link>
-    <router-link to="/settings/" class="px-4 cursor-pointer">
-      Settings
-    </router-link>
+    <router-link to="/" class="px-4 cursor-pointer">Home</router-link>
+    <router-link to="/settings/" class="px-4 cursor-pointer"
+      >Settings</router-link
+    >
   </div>
 </template>
 
